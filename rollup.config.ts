eb29e1320a3cef95f5b14f0a9f9778ce79f82a90
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2022-06-01 16:59:47 +0200

    fix(bundling): fix bundling to expose cjs and es
    Swapped umd and cjs, this lib does not need browser support

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,7 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import json from '@rollup/plugin-json';
-import { camelCase } from 'lodash';
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
@@ -11,7 +10,7 @@ const libraryName = 'ksqldb-graphql'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
